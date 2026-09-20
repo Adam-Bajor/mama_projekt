9de71733aee4fa5c9f9beec48fcf83936fcfdbdc
Adam-Bajor <adamb110055@gmail.com>
2026-09-20 19:08:38 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 window.TABLETKI_CONFIG = {
-    apiUrl: "https://ngfndqujwgwmnmnunwcv.supabase.co/functions/v1/smooth-action"
+    const apiUrl = "https://rpjflpbfmyvusxlvmyal.supabase.co/functions/v1/clever-handler";
 };
